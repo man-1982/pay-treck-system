ad9ff2ece23831a3500693a70f5c9f323220d68d
man-1982 <man-1982@yandex.ru>
2024-04-14 16:28:56 -0700

    added projects

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the project that owns the task.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function projects(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
