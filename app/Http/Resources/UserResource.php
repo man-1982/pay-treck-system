56b9c734dd267dfacef1e03fdf3d27dd7b2fdda0
man-1982 <man-1982@yandex.ru>
2024-04-21 14:57:16 -0700

    prepared use resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,14 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // TODO add verification for user roles and base on this roles return different data
+        return [
+            'id'                => $this->id,
+            'name'              => $this->name,
+            'email'             => $this->email,
+            'emailVerifiedAt'   => $this->email_verified_at,
+            'createdAt'         => Carbon::parse($this->created_at)->format('d-m-Y'),
+            'updatedAt'         => Carbon::parse($this->updated_at)->format('d-m-Y'),
+        ];
     }
 }
