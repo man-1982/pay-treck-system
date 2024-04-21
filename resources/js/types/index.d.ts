c2eb5f2b6e170b85ed9a05adb3b0ffcc971afbfa
man-1982 <man-1982@yandex.ru>
2024-04-21 14:51:46 -0700

    define types for Projects

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,8 +5,27 @@ export interface User {
     email_verified_at: string;
 }
 
+
+
+interface Project {
+    id: number;
+    name: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+    status: string;
+    imagePath: string;
+    createdBy: User;
+    updatedBy: User;
+}
+
+interface Projects {
+    data: Project[];
+}
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
+    projects: Projects;
 };
