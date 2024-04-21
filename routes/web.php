29324296d9f2d0d237fe36a06005e19316cbd7dc
man-1982 <man-1982@yandex.ru>
2024-04-21 14:52:54 -0700

    defined groups for routers

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,30 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserControler;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// TODO devide routes into goups and add API prefix
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::redirect('/', '/dashboard');
+
+Route::middleware(['auth', 'verified'])->group(
+    function (){
+        Route::get('/dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+
+        //index, create, store, show, edit, update, and destroy.
+        Route::resource('projects', ProjectController::class);
+        Route::resource('tasks', TaskController::class);
+        Route::resource('users', UserControler::class);
+    }
+);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -24,4 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
