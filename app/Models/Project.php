887898c9fa9db8637c36f2aed555a313cd763ec0
man-1982 <man-1982@yandex.ru>
2024-04-21 14:57:52 -0700

    Updated Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,8 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    /**
+     * Define the task relationship.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function tasks(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Task::class);
     }
+
+
+    /**
+     * Define created_by relationship.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function createdBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    /**
+     * Define updated_by relationship.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function updatedBy(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+
 }
