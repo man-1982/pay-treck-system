a19f084878e78d63a530334001fc5c13b5b83d08
man-1982 <man-1982@yandex.ru>
2024-05-03 15:06:30 -0700

    updated types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,3 +1,5 @@
+import {RouteParams, RouteParamsObject} from "ziggy-js";
+
 export interface User {
     id: number;
     name: string;
@@ -13,19 +15,49 @@ interface Project {
     description: string;
     createdAt: string;
     updatedAt: string;
+    dueDate: string;
     status: string;
     imagePath: string;
     createdBy: User;
     updatedBy: User;
 }
 
+export interface Pagination {
+    current_page: number;
+    from: number;
+    last_page: number;
+    links: Link[];
+    path: string;
+    per_page: number;
+    to: number;
+    total: number;
+}
+
 interface Projects {
     data: Project[];
+    links: [];
+    meta: Pagination;
 }
+
+interface Link {
+    url: string | null;
+    label: string;
+    active: boolean;
+}
+
+type ExtendedRouteParams = RouteParams<string> & {
+    name?: string;
+    status?: string;
+    page?: number;
+    sort_field?: string;
+    sort_direction?: string;
+};
+
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
     projects: Projects;
+    queryParams?: ExtendedRouteParams | undefined;
 };
